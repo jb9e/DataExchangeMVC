293e493b1601accb94bd77dca739daa044836edc
jeff bryant <jb9e@hotmail.com>
2012-11-07 19:48:04 -0700

    DataExchangeMVC Initial Commit

---
+++ DataExchangeMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataExchangeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to the Data Exchange Tool.";
+            ViewBag.NumTimes = this.HttpContext.Application["SiteCounter"];
+            if (Session["LastPersonQuery"] != null)
+            {
+                ViewBag.LastPersonQuery = "The last person queried was: " + Session["LastPersonQuery"]; 
+            }
+            else
+            {
+                ViewBag.LastPersonQuery = "No person queries attempted yet.";
+            }
+            return View();
+        }
+
+        //public ActionResult Welcome(string name, int numTimes = 1)
+        //{
+        //    ViewBag.Message = "Hello " + name;
+        //    ViewBag.NumTimes = numTimes;
+
+        //    return View();
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This tool does not do much.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact information.";
+
+            return View();
+        }
+    }
+}
---
+++ DataExchangeMVC/Controllers/VehiclesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataExchangeMVC.Models;
+
+namespace DataExchangeMVC.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private DataExchangeDBContext db = new DataExchangeDBContext();
+
+        //
+        // GET: /Vehicles/
+
+        public ActionResult Index()
+        {
+            return View(db.Vehicles.ToList());
+        }
+
+        //
+        // GET: /Vehicles/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        //
+        // GET: /Vehicles/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Vehicles/Create
+
+        [HttpPost]
+        public ActionResult Create(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vehicles.Add(vehicle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        //
+        // GET: /Vehicles/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        //
+        // POST: /Vehicles/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vehicle);
+        }
+
+        //
+        // GET: /Vehicles/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        //
+        // POST: /Vehicles/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            db.Vehicles.Remove(vehicle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
